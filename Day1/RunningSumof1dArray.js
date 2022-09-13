12127432a692ec72c361e55e8a56667cc0c29156
choi1five <99406837+choi1five@users.noreply.github.com>
2022-09-13 17:40:47 +0900

    wonoh solution

--- Day1/RunningSumof1dArray.js
+++ Day1/RunningSumof1dArray.js
@@ -24,6 +24,18 @@ var hwangRunningSum = function (nums) {
  * @param {number[]} nums
  * @return {number[]}
  */
-const parkRunningSum = nums => {
-  return nums.map((num, index, nums) => nums.filter((_num, _index) => index >= _index).reduce((acc, cur) => acc + cur));
+const parkRunningSum = (nums) => {
+  return nums.map((num, index, nums) =>
+    nums
+      .filter((_num, _index) => index >= _index)
+      .reduce((acc, cur) => acc + cur)
+  );
 };
+
+//* 최원오 시작 ----------------------------------------------------------------------------------------------------------------------------------
+const choiRunningSum = function (nums) {
+  return nums.map((num, i) =>
+    nums.slice(0, i + 1).reduce((acc, cur) => acc + cur, 0)
+  );
+};
+//* 최원오 끝 ----------------------------------------------------------------------------------------------------------------------------------
