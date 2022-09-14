2eedec5405f51458d47a9bf2056fcef60397ed34
dee0518 <dehya-518@naver.com>
2022-09-14 09:35:16 +0900

    :memo: doeun 중복 함수 제거

--- Day01/RunningSumof1dArray.js
+++ Day01/RunningSumof1dArray.js
@@ -9,19 +9,6 @@ const jeongRunningSum = function (nums) {
   return resultArr;
 };
 
-// doeun solution
-const hwangRunningSum = function (nums) {
-  return nums.map(
-    (num, i) =>
-      num +
-      nums.reduce((acc, cur, idx) => {
-        if (idx < i) return acc + cur;
-        return acc;
-      }, 0)
-  );
-};
-
-console.log(hwangRunningSum([1, 2, 3, 4]));
 /**
  * @param {number[]} nums
  * @return {number[]}
@@ -56,3 +43,5 @@ const hwangRunningSum = function (nums) {
     }, 0)
   );
 };
+
+console.log(hwangRunningSum([1, 2, 3, 4]));
