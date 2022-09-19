c4906dee967995ff543693fb92746dfc2c4de120
Dabin <kdbj12003@gmail.com>
2022-09-19 12:23:22 +0900

    binarySearch day7

---
+++ Day07/BinarySearch.js
@@ -0,0 +1,19 @@
+//Dabin's solution ----------------------------------------------------------------------------------------
+var kimSearch = function(nums, target) {
+  let mid = 0;
+  let left = 0;
+  let right = nums.length-1;
+  
+  while(left<=right){
+      mid = Math.floor(left + right)/2
+      if(nums[mid] === target) return mid;
+      
+      if(nums[mid]>target){
+          right = mid - 1;
+      }else{
+          left = mid + 1
+      }
+  }
+  return -1
+};
+//End of Dabin ----------------------------------------------------------------------------------------
