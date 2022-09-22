0899eb83a419dd4c89c510731a1fb47d4ddc8bf7
wonoh <cwo1401@gmail.com>
2022-09-22 10:14:31 +0900

    day 9 wonoh

--- Day09/ImplementQueueusingStacks.js
+++ Day09/ImplementQueueusingStacks.js
@@ -1,36 +1,71 @@
 // * doeun --------------------------------------------------------------------------------------------------------------------//
-const HwangMyQueue = function() {
+const HwangMyQueue = function () {
   this.array = [];
 };
 
-/** 
-* @param {number} x
-* @return {void}
-*/
-HwangMyQueue.prototype.push = function(x) {
-  this.array.push(x)
+/**
+ * @param {number} x
+ * @return {void}
+ */
+HwangMyQueue.prototype.push = function (x) {
+  this.array.push(x);
 };
 
 /**
-* @return {number}
-*/
-HwangMyQueue.prototype.pop = function() {
+ * @return {number}
+ */
+HwangMyQueue.prototype.pop = function () {
   const val = this.array.shift();
-  
+
   return val;
 };
 
 /**
-* @return {number}
-*/
-HwangMyQueue.prototype.peek = function() {
+ * @return {number}
+ */
+HwangMyQueue.prototype.peek = function () {
   return this.array[0];
 };
 
 /**
-* @return {boolean}
-*/
-HwangMyQueue.prototype.empty = function() {
+ * @return {boolean}
+ */
+HwangMyQueue.prototype.empty = function () {
   return this.array.length === 0;
 };
 // * doeun end--------------------------------------------------------------------------------------------------------------------//
+
+//* 최원오 -----------------------------------------------------------------------------
+
+const choiMyQueue = function () {
+  this.queue = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+choiMyQueue.prototype.push = function (x) {
+  this.queue.push(x);
+};
+
+/**
+ * @return {number}
+ */
+choiMyQueue.prototype.pop = function () {
+  return this.queue.splice(0, 1);
+};
+
+/**
+ * @return {number}
+ */
+choiMyQueue.prototype.peek = function () {
+  return this.queue[0];
+};
+
+/**
+ * @return {boolean}
+ */
+choiMyQueue.prototype.empty = function () {
+  return this.queue.length === 0;
+};
