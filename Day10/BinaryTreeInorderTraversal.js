5c4e38c2a3fbd75bd48821accba38f9d7cb5d56c
harseille <joonhabaak@gmail.com>
2022-09-24 12:06:31 +0900

    :sparkles: solve day 10 problems

--- Day10/BinaryTreeInorderTraversal.js
+++ Day10/BinaryTreeInorderTraversal.js
@@ -1,14 +1,25 @@
 // * doeun --------------------------------------------------------------------------------------------------------------------//
-const hwangInorderTraversal = function(root) {
+const hwangInorderTraversal = function (root) {
   const inorder = (r = root, arr) => {
-      if(!r) return [];
-      
-      inorder(r.left, arr);
-      arr.push(r.val);
-      inorder(r.right,arr);
-      return arr;
-  }
-  
-  return inorder(root, [])
+    if (!r) return [];
+
+    inorder(r.left, arr);
+    arr.push(r.val);
+    inorder(r.right, arr);
+    return arr;
+  };
+
+  return inorder(root, []);
 };
 // * doeun end--------------------------------------------------------------------------------------------------------------------//
+
+/**
+ * * Park ******************************************************************************************
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const parkInorderTraversal = function (root) {
+  if (!root) return [];
+
+  return [...parkInorderTraversal(root.left), root.val, ...parkInorderTraversal(root.right)];
+};
