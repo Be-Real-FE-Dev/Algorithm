f42b163d0a74a0fe17e9f314001cee5d264ba991
Park <85827017+harseille@users.noreply.github.com>
2022-09-26 11:16:10 +0900

    Update InsertintoaBinarySearchTree.js

--- Day13/InsertintoaBinarySearchTree.js
+++ Day13/InsertintoaBinarySearchTree.js
@@ -1,17 +1,18 @@
 /**
+ * * Park *************************************************
  * @param {TreeNode} root
  * @param {number} val
  * @return {TreeNode}
  */
 
-const insertIntoBST = function (root, val) {
+const parkInsertIntoBST = function (root, val) {
   if (!root) {
     return new TreeNode(val);
   }
   if (root.val > val) {
-    root.left = insertIntoBST(root.left, val);
+    root.left = parkInsertIntoBST(root.left, val);
   } else if (root.val < val) {
-    root.right = insertIntoBST(root.right, val);
+    root.right = parkInsertIntoBST(root.right, val);
   }
   return root;
 };
