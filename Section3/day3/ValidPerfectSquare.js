f07a2572f03bf0d97f7f046f9fd0c65b2ab55a4f
wonoh <cwo1401@gmail.com>
2022-10-22 18:08:32 +0900

    day3 wonoh

--- Section3/day3/ValidPerfectSquare.js
+++ Section3/day3/ValidPerfectSquare.js
@@ -22,3 +22,21 @@ const hwangIsPerfectSquare = function (num) {
 const parkIsPerfectSquare = function (num) {
   return Number.isInteger(Math.sqrt(num));
 };
+
+// * wonoh -------------------------------------------------------
+
+const choiIsPerfectSquare = function (num) {
+  let start = 1;
+  let end = Math.ceil(num / 2);
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (mid ** 2 === num) return true;
+
+    if (mid ** 2 < num) start = mid + 1;
+    else end = mid - 1;
+  }
+
+  return false;
+};
