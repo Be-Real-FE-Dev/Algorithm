3e2d16ae3724fa743fb9925198988860c909d94e
dee0518 <dehya-518@naver.com>
2022-10-25 09:50:15 +0900

    :tada: doeun day12 문제 풀이 완료

---
+++ Section3/day12/FindMinimumInRotatedSorted.js
@@ -0,0 +1,14 @@
+// * doeun --------------------------------------------------------------------------------------------------------------------//
+const hwangfindMin = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[right] >= nums[mid]) right = mid;
+    else left = mid + 1;
+  }
+
+  return nums[left];
+};
