8d184e82dca555cdeb8793c8e53a07f607c2a206
choi1five <cwo1401@gmail.com>
2022-12-19 14:19:02 +0900

    day2 wonoh

--- Dynamic Programming I/day2/70.ClimbingStairs.js
+++ Dynamic Programming I/day2/70.ClimbingStairs.js
@@ -8,3 +8,15 @@ const doeunclimbStairs = function(n) {
 
   return dp[n]
 };
+
+//* wonoh
+
+const choiClimbStairs = function(n) {
+  const dp = [0, 1, 2];
+
+for(let i = 3; i <= n; i++){
+   dp[i] = dp[i - 1] + dp[i - 2];
+}
+
+return dp[n]
+};
--- Dynamic Programming I/day2/746.MinCostClimbingStairs.js
+++ Dynamic Programming I/day2/746.MinCostClimbingStairs.js
@@ -8,3 +8,16 @@ const hwangminCostClimbingStairs = function(cost) {
   
   return Math.min(dp[cost.length - 1], dp[cost.length - 2])
 };
+
+//* wonoh
+
+const choiMinCostClimbingStairs = function(cost) {
+  const dp = [cost[0], cost[1]];
+  
+  for (let i = 2; i < cost.length; i++){
+      dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i];
+  }
+
+  return Math.min(dp[cost.length-1], dp[cost.length-2] );
+
+};
