47cb34408135a195f32c2fe7c7136bc6f5bb99d8
harseille <joonhabaak@gmail.com>
2022-12-22 09:02:47 +0900

    :sparkles: solve dp day4 problem 1

--- Dynamic Programming I/day4/45.JumpGameII.js
+++ Dynamic Programming I/day4/45.JumpGameII.js
@@ -1,17 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const canJump = function (nums) {
+  let max = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (max < i) return false;
+    max = Math.max(max, i + nums[i]);
+    if (max >= nums.length - 1) return true;
+  }
+};
+
 //* doeun ----------------------------------------------------------------------------------------------------------------------------------
-const hwangjump = function(nums) {
+const hwangjump = function (nums) {
   let count = 0;
   let max = 0;
   let oldMax = 0;
 
-  for(let i = 0; i < nums.length - 1; i++){
-      max = Math.max(max, nums[i] + i);
+  for (let i = 0; i < nums.length - 1; i++) {
+    max = Math.max(max, nums[i] + i);
 
-      if(i === oldMax) {
-          count += 1;
-          oldMax = max;
-      }
+    if (i === oldMax) {
+      count += 1;
+      oldMax = max;
+    }
   }
 
-  return count
+  return count;
 };
--- Dynamic Programming I/day4/55.JumpGame.js
+++ Dynamic Programming I/day4/55.JumpGame.js
@@ -1,11 +1,24 @@
 //* doeun ----------------------------------------------------------------------------------------------------------------------------------
-const hwangcanJump = function(nums) {
+const hwangcanJump = function (nums) {
   let max = 0;
 
-  for(let i = 0; i < nums.length; i++){
-      if(max < i) return false;
-      max = Math.max(max, nums[i] + i)
+  for (let i = 0; i < nums.length; i++) {
+    if (max < i) return false;
+    max = Math.max(max, nums[i] + i);
   }
 
   return true;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const park_canJump = function (nums) {
+  let maxIndex = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (maxIndex < i) return false;
+    maxIndex = Math.max(maxIndex, i + nums[i]);
+    if (maxIndex >= nums.length - 1) return true;
+  }
 };
