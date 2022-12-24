3edb3f510128e037715e58c5bd99e1cfb1d6b2f8
choi1five <cwo1401@gmail.com>
2022-12-24 22:36:07 +0900

    day7 wonoh

--- Dynamic Programming I/day7/122.BestTimeToBuyandSellStock2.js
+++ Dynamic Programming I/day7/122.BestTimeToBuyandSellStock2.js
@@ -8,3 +8,15 @@ const hwangmaxProfit = function(prices) {
 
   return maxProfit;
 };
+
+//* wonoh
+
+const choiMaxProfit = function (prices) {
+  let result = 0;
+  let min = prices[0];
+  for (let i = 1; i < prices.length; i++) {
+      min = Math.min(prices[i], min);
+      result = Math.max(result, prices[i] - min);
+  }
+  return result;
+};
