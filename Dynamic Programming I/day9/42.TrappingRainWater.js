985aaedb270d6561aa7af1e271177405bc108d6a
choi1five <cwo1401@gmail.com>
2022-12-27 22:35:18 +0900

    day9 wonoh

--- Dynamic Programming I/day9/42.TrappingRainWater.js
+++ Dynamic Programming I/day9/42.TrappingRainWater.js
@@ -40,3 +40,24 @@ const park_wordBreak = (s, wordDict) => {
 
   return dp[s.length];
 };
+
+
+const choiTrap =  function(height) {
+  let answer = 0;
+  
+  let toRight = new Array(height.length);
+  let toLeft = new Array(height.length);
+  
+  toRight[0] = height[0];
+  toLeft[height.length-1] = height[height.length-1];
+  
+  for(let i = 1; i < height.length; i++){
+      toRight[i] = Math.max(height[i],  toRight[i-1]);
+      toLeft[height.length-1-i] = Math.max(height[height.length-1-i], toLeft[height.length-i]);
+  }
+  
+  for(let i = 0; i < height.length; i++)
+      answer += Math.min(toLeft[i], toRight[i]) - height[i];
+  
+  return answer;
+};
