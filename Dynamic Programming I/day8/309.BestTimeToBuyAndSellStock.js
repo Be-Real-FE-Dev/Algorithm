20bbfa267e9136c621161e46a54d3468d49d7305
dee0518 <dehya-518@naver.com>
2022-12-27 10:52:24 +0900

    :tada: day 9 완료

--- Dynamic Programming I/day8/309.BestTimeToBuyAndSellStock.js
+++ Dynamic Programming I/day8/309.BestTimeToBuyAndSellStock.js
@@ -5,7 +5,6 @@ const hwangmaxProfit = function (prices) {
   let prev_profit = 0;
 
   for (let i = 1; i < prices.length; i++) {
-    console.log(selling, profit, prev_profit);
     selling = Math.max(selling, prev_profit - prices[i]);
     prev_profit = profit;
     profit = Math.max(profit, selling + prices[i]);
