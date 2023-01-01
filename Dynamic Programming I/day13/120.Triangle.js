3b38621efeef22eafe0484663db837c9bf0adc54
harseille <joonhabaak@gmail.com>
2023-01-01 23:09:09 +0900

    :sparkles: solve dp day14

--- Dynamic Programming I/day13/120.Triangle.js
+++ Dynamic Programming I/day13/120.Triangle.js
@@ -10,3 +10,23 @@ const park_minimumTotal = function (triangle) {
   }
   return triangle[0][0];
 };
+
+const you = new Map();
+
+const getUntilNewYear = (yourName, yourWishCompanyName, newYear = '2023') => {
+  console.log(yourName + '~ 새해 복 많이 받으세요');
+
+  setInterval(() => {
+    if (new Date() <= new Date(newYear, '11', '31', 23, 59, 59)) {
+      you.set('happiness', you.get('happiness') || Infinity);
+      you.set('love', you.get('love') || Infinity);
+      you.set('Wellfare', you.get('Wellfare') || Infinity);
+      you.set('money', you.get('money') || Infinity);
+      you.set('job', you.get('job') || yourWishCompanyName);
+
+      you.clear('worry');
+      you.clear('envy');
+      you.clear('badMemory');
+    }
+  }, 1000);
+};
