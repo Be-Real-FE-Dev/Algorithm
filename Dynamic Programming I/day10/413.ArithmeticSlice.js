9d9e24ff1a5e1d03c7335d6eb8d6b5d202e5a0b8
Dada K <dadak@Dadas-MacBook-Air.local>
2023-03-13 08:39:25 +0900

    20230313(mon) 일곱난장이

--- Dynamic Programming I/day10/413.ArithmeticSlice.js
+++ Dynamic Programming I/day10/413.ArithmeticSlice.js
@@ -1,17 +1,11 @@
 // //* wonoh
 
-<<<<<<< HEAD
 const choiNumberOfArithmeticSlices = function(nums) {
   if(numlength < 3) return 0;
-=======
-const choiNumberOfArithmeticSlices = function (nums) {
-  if (nums.length < 3) return 0;
->>>>>>> origin/main
 
   let ans = 0;
   let cnt = 0;
 
-<<<<<<< HEAD
 for (let i = 2; i < numlength; i++){
     if(nums[i] - nums[i-1] === nums[i-1] - nums[i-2]){
         cnt += 1;
@@ -23,31 +17,3 @@ return ans;
 };
 
 
-=======
-  for (let i = 2; i < nums.length; i++) {
-    if (nums[i] - nums[i - 1] === nums[i - 1] - nums[i - 2]) {
-      cnt += 1;
-      ans += cnt;
-    } else cnt = 0;
-  }
-
-  return ans;
-};
-
-/**
- * @param {number[]} nums
- * @return {number}
- */
-const park_numberOfArithmeticSlices = function (nums) {
-  if (nums.length < 3) return 0;
-
-  const dp = Array(nums.length).fill(0);
-
-  for (let i = 2; i < nums.length; i++) {
-    if (nums[i] - nums[i - 1] === nums[i - 1] - nums[i - 2]) {
-      dp[i] = dp[i - 1] + 1;
-    }
-  }
-  return dp.reduce((cur, acc) => cur + acc);
-};
->>>>>>> origin/main
