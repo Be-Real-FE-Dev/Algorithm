da2a67fd5c7eea02bfa956d23cb6819fe91bf955
choi1five <cwo1401@gmail.com>
2023-03-15 21:50:49 +0900

    climbing-stairs

---
+++ programmers/level1/climbing-stairs.js
@@ -0,0 +1,18 @@
+const climbingStairs = n => {
+  if (n === 0){
+    return 0;
+  }
+
+  let dp = [];
+
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i=3; i <=n; i++){
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+}
+
+console.log(climbingStairs(5))
