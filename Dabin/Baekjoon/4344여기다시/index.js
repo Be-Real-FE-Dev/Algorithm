53c29702606b98b7643f7b1a9a443879d2094943
Dada K <dadak@Dadas-MacBook-Air.local>
2023-04-19 22:39:28 +0900

    백준 평균은 넘겠지

--- Dabin/Baekjoon/4344여기다시/index.js
+++ Dabin/Baekjoon/4344여기다시/index.js
@@ -1,11 +1,16 @@
-// const input = require('fs')
-//   // eslint-disable-next-line no-path-concat
-//   .readFileSync(__dirname + '/input.txt')
-//   .toString().split('\n')
+const input = require('fs')
+  // eslint-disable-next-line no-path-concat
+  .readFileSync(__dirname + '/input.txt')
+  .toString()
+  .split('\n');
 
+for (let i = 1; i < input.length - 1; i++) {
+  const eachArr = input[i].split(' ').map(Number);
+  const avg = (eachArr.reduce((a, b) => a + b) - eachArr[0]) / eachArr[0];
 
-//   for(let i=1; i<=input.length; i++){
-//    let eachArr = (input[i]||'').split(' ').map(Number);
-//    eachArr.shift();
-//    console.log(eachArr)
-//   }
+  let count = 0;
+  for (let i = 1; i <= eachArr[0]; i++) {
+    if (eachArr[i] > avg) count += 1;
+  }
+  console.log(`${((count / eachArr[0]) * 100).toFixed(3)}%`);
+}
