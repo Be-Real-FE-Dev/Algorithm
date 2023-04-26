eedae4d7c086e4a47802c9296b8b30bc0173ad47
Dada K <dadak@Dadas-MacBook-Air.local>
2023-04-26 22:31:48 +0900

    백준 문자열

--- Dabin/Baekjoon/1316/index.js
+++ Dabin/Baekjoon/1316/index.js
@@ -1,28 +1,26 @@
-// const input = require('fs')
-//   // eslint-disable-next-line no-path-concat
-//   .readFileSync(__dirname + '/input.txt')
-//   .toString()
-//   .split('\n');
+/* eslint-disable no-use-before-define */
+const input = require('fs')
+  // eslint-disable-next-line no-path-concat
+  .readFileSync(__dirname + '/input.txt')
+  .toString()
+  .split('\n');
 
-// let count = 0;
+function check(data) {
+  const setData = new Set(data[0]); // 각 알파벳 개수
+  for (let i = 0; i < data.length - 1; i++) {
+    if (data[i] !== data[i + 1]) {
+      if (setData.has(data[i + 1])) {
+        return false;
+      }
+      setData.add(data[i + 1]);
+    }
+  }
+  return true;
+}
 
-// function check(data) {
-//   const setData = new Set(data);
-
-//   for (let i = 0; i < data.length - 1; i++) {
-//     if (data[i] !== data[i + 1]) {
-//       if (setData.has(data[i + 1])) {
-//         return false;
-//       }
-//       setData.add(data[i + 1]);
-//     }
-//   }
-//   return true;
-// }
-
-// for (let i = 1; i <= +input[0]; i++) {
-//   const data = input[i];
-//   if (check(data)) count += 1;
-// }
-
-// console.log(count);
+let count = 0;
+for (let i = 1; i <= +input[0]; i++) {
+  const data = input[i];
+  if (check(data)) count += 1;
+}
+console.log(count);
